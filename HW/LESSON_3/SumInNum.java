3e589a7eef9c754dd607fa1b63956c4a3c37c764
S <7438124@gmail.com>
2017-02-13 12:01:44 +0200

    TEST

--- HW/LESSON_3/SumInNum.java
+++ HW/LESSON_3/SumInNum.java
@@ -4,5 +4,14 @@
 import java.util.Scanner;
 
 public class SumInNum {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Input Intager Value: ");
+        String nUmberString = scan.next();
+        int sUmNumOfSting = 0;
+        for (int i = (nUmberString.length()- 1); i>=0; i--){
+            sUmNumOfSting = sUmNumOfSting + Character.getNumericValue(nUmberString.charAt(i));
+        }
+        System.out.print("Sum is: "+sUmNumOfSting);
+    }
 }
