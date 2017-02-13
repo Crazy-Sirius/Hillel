dd7b953d842f8163e68cfe395c575c3c2e6e696b
S <7438124@gmail.com>
2017-02-13 17:12:30 +0200

    TEST

--- HW/LESSON_5/MassaGram.java
+++ HW/LESSON_5/MassaGram.java
@@ -1,17 +1,21 @@
+import java.lang.System;
+import java.util.Scanner;
+
 /**
  * Created by admin on 13.02.2017.
  */
 public class MassaGram {
     public static void main(String[] args) {
-        int massaGramm = 145852312;
+        Scanner scan = new Scanner(System.in);
+        int massaGramm = scan.nextInt();
         int buf = massaGramm;
         int bufN =0;
         int k = 0;
         int [] Poryadok;
         Poryadok  = new int [3];
         String str = "";
-        while (buf >0){
 
+        while (buf >0){
           Poryadok[k] = buf % 1000;
           buf = buf / 1000;
           k = k + 1;
@@ -31,6 +35,6 @@ public class MassaGram {
         if (Poryadok[0]!=0) {
             str = str + Poryadok[0] + " gramms ";
         }
-        System.out.print(str);
+      System.out.print(str);
     }
 }
