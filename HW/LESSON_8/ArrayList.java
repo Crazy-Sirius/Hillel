2eb4ce098112fe35878a0f4651b3dda23fc6f9aa
7438124@gmail.com <7438124@gmail.com>
2017-03-06 16:28:51 +0200

    first

--- HW/LESSON_8/ArrayList.java
+++ HW/LESSON_8/ArrayList.java
@@ -5,12 +5,19 @@ import java.awt.*;
 /**
  * Created by toor on 06.03.17.
  */
-public class ArrayList extends Object{
-    private int [] arrayList;
-    public ArrayList(){
+public class ArrayList {
+    private Object[] list;
+    private int size;
 
+    public ArrayList() {
+        this.size = 0;
+        this.list = new Object[10];
     }
     public int getSize(Object o){
+        return size;
+    }
+
+    public int getElIndex(Object o){
 
     }
 
