397eda104526d653d6ef2b4f0c0d19a918a3205c
7438124@gmail.com <7438124@gmail.com>
2017-03-06 16:43:17 +0200

    first

--- HW/LESSON_8/ArrayList.java
+++ HW/LESSON_8/ArrayList.java
@@ -10,15 +10,29 @@ public class ArrayList {
     private int size;
 
     public ArrayList() {
-        this.size = 0;
+        this.size = 10;
         this.list = new Object[10];
     }
     public int getSize(Object o){
         return size;
     }
 
-    public int getElIndex(Object o){
+    public int getElIndex(Object o,){
+        return this.list.;
+    }
+    public void setEl(Object o){
+        Object[] newList = new Object[size + 1];
+        for (int i = 0; i < size; i++) {
+            newList[i] = list[i];
+        }
+        newList[size] = o;
+        list = newList;
+        size++;
 
     }
 
+    public static void main(String[] args) {
+        ArrayList test = new ArrayList();
+        System.out.print(test.list.length);
+    }
 }
