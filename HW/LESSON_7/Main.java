9b7f8b5b3afbb2b6b15f7f8228c6b9adc083ba60
7438124@gmail.com <7438124@gmail.com>
2017-03-06 13:28:53 +0200

    &&  ->  &

--- HW/LESSON_7/Main.java
+++ HW/LESSON_7/Main.java
@@ -3,12 +3,5 @@
  */
 
 public class Main {
-    public static void main(String[] args) {
-        Container containerTest = new Container();
-        int [] s = {1,2,3,4,5,6};
-        containerTest.addArrayToContainer(s);
-        containerTest.add(12);
-        System.out.println(containerTest.getElement(0));
-        System.out.print(containerTest.getElement(7));
-    }
+
 }
