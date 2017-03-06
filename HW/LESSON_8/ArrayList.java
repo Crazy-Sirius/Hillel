32840dcaf865f9675ba8879430bddfd91823b06b
7438124@gmail.com <7438124@gmail.com>
2017-03-06 15:53:27 +0200

    first

---
+++ HW/LESSON_8/ArrayList.java
@@ -0,0 +1,17 @@
+package LESSON_8;
+
+import java.awt.*;
+
+/**
+ * Created by toor on 06.03.17.
+ */
+public class ArrayList extends Object{
+    private int [] arrayList;
+    public ArrayList(){
+
+    }
+    public int getSize(Object o){
+
+    }
+
+}
